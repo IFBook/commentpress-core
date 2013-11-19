56a5bcb517e90e25f43fa31bab7f36a5a7d16c2a
Christian Wach <needle@haystack.co.uk>
2013-11-19 09:47:11 +0000

    upgrade compose screen code

--- themes/commentpress-modern/members/single/messages/compose.php
+++ themes/commentpress-modern/members/single/messages/compose.php
@@ -5,30 +5,30 @@
 	<label for="send-to-input"><?php _e("Send To (Username or Friend's Name)", 'commentpress-core') ?></label>
 	<ul class="first acfb-holder">
 		<li>
-			<?php bp_message_get_recipient_tabs() ?>
+			<?php bp_message_get_recipient_tabs(); ?>
 			<input type="text" name="send-to-input" class="send-to-input" id="send-to-input" />
 		</li>
 	</ul>
 
-	<?php if ( is_super_admin() ) : ?>
-		<input type="checkbox" id="send-notice" name="send-notice" value="1" /> <?php _e( "This is a notice to all users.", "buddypress" ) ?>
+	<?php if ( bp_current_user_can( 'bp_moderate' ) ) : ?>
+		<input type="checkbox" id="send-notice" name="send-notice" value="1" /> <?php _e( "This is a notice to all users.", "buddypress" ); ?>
 	<?php endif; ?>
 
 	<label for="subject"><?php _e( 'Subject', 'commentpress-core') ?></label>
-	<input type="text" name="subject" id="subject" value="<?php bp_messages_subject_value() ?>" />
+	<input type="text" name="subject" id="subject" value="<?php bp_messages_subject_value(); ?>" />
 
 	<label for="content"><?php _e( 'Message', 'commentpress-core') ?></label>
-	<textarea name="content" id="message_content" rows="15" cols="40"><?php bp_messages_content_value() ?></textarea>
+	<textarea name="content" id="message_content" rows="15" cols="40"><?php bp_messages_content_value(); ?></textarea>
 
-	<input type="hidden" name="send_to_usernames" id="send-to-usernames" value="<?php bp_message_get_recipient_usernames(); ?>" class="<?php bp_message_get_recipient_usernames() ?>" />
+	<input type="hidden" name="send_to_usernames" id="send-to-usernames" value="<?php bp_message_get_recipient_usernames(); ?>" class="<?php bp_message_get_recipient_usernames(); ?>" />
 
-	<?php do_action( 'bp_after_messages_compose_content' ) ?>
+	<?php do_action( 'bp_after_messages_compose_content' ); ?>
 
 	<div class="submit">
-		<input type="submit" value="<?php _e( "Send Message", 'commentpress-core' ) ?>" name="send" id="send" />
+		<input type="submit" value="<?php _e( "Send Message", 'commentpress-core' ); ?>" name="send" id="send" />
 	</div>
 
-	<?php wp_nonce_field( 'messages_send_message' ) ?>
+	<?php wp_nonce_field( 'messages_send_message' ); ?>
 </form>
 
 <script type="text/javascript">
