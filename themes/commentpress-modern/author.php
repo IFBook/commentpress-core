246d09748bbc8e99e3f90cc0450f30889b3d225c
Christian Wach <needle@haystack.co.uk>
2014-11-11 17:53:43 +0000

    pinking shears on overlooked files

--- themes/commentpress-modern/author.php
+++ themes/commentpress-modern/author.php
@@ -19,7 +19,7 @@ $authorURL = '';
 
 // do we have an URL for this user?
 if ( $my_author->user_url != '' AND $my_author->user_url != 'http://' ) {
-	
+
 	// set url
 	$authorURL = $my_author->user_url;
 
@@ -55,10 +55,10 @@ get_header(); ?>
 <h2 class="post_title"><?php echo $my_author->nickname; ?></h2>
 <?php } ?>
 
-<?php 
+<?php
 
 // get avatar
-$_avatar = get_avatar( $my_author->user_email, $size='200' ); 
+$_avatar = get_avatar( $my_author->user_email, $size='200' );
 
 // did we get one?
 if ( $_avatar != '' ) {
@@ -78,7 +78,7 @@ if ( $_avatar != '' ) {
 
 
 
-<?php 
+<?php
 
 /*
 
@@ -142,24 +142,24 @@ if ( $my_name != '' ) { ?>
 
 	// init name
 	$_author_name = $my_author->nickname;
-	
+
 	// override if there's a display name
-	if ( $my_author->display_name != '' ) { 
+	if ( $my_author->display_name != '' ) {
 		$_author_name = $my_author->display_name;
 	}
-	
+
 	?>
-	<h3 class="author_pages_heading"><?php 
-		
+	<h3 class="author_pages_heading"><?php
+
 		// show title
-		echo apply_filters( 'cp_author_page_posts_list_title', __( 'Posts written by', 'commentpress-core' ) ); 
-		
+		echo apply_filters( 'cp_author_page_posts_list_title', __( 'Posts written by', 'commentpress-core' ) );
+
 	?> <?php echo $_author_name; ?></h3>
-	
-	
+
+
 
 	<ul class="author_pages">
-	
+
 	<?php while ( have_posts() ) : the_post(); ?>
 
 		<li>
@@ -192,30 +192,30 @@ $_pages = new WP_Query( $page_args );
 if ( $_pages->have_posts() ) {
 
 	//print_r( $pages ); die();
-	
+
 	// init name
 	$_author_name = $my_author->nickname;
-	
+
 	// override if there's a display name
-	if ( $my_author->display_name != '' ) { 
+	if ( $my_author->display_name != '' ) {
 		$_author_name = $my_author->display_name;
 	}
-	
+
 	?>
-	<h3 class="author_pages_heading"><?php 
-		
+	<h3 class="author_pages_heading"><?php
+
 		// show title
-		echo apply_filters( 'cp_author_page_pages_list_title', __( 'Pages written by', 'commentpress-core' ) ); 
-		
+		echo apply_filters( 'cp_author_page_pages_list_title', __( 'Pages written by', 'commentpress-core' ) );
+
 	?> <?php echo $_author_name; ?></h3>
-	
-	
+
+
 	<ul class="author_pages">
 	<?php
-	
+
 	// let's roll
 	while ( $_pages->have_posts() ) : $_pages->the_post(); ?>
-	
+
 		<li>
 		<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
 		</li>
--- themes/commentpress-modern/footer.php
+++ themes/commentpress-modern/footer.php
@@ -7,36 +7,36 @@
 
 
 
-<div id="footer">	
+<div id="footer">
 
 <div id="footer_inner">
 
-	<?php 
-	
+	<?php
+
 	// are we using the page footer widget?
 	if ( !dynamic_sidebar( 'cp-license-8' ) ) {
-		
+
 		// no - make other provision here
-	
+
 		// compat with wplicense plugin
 		if ( function_exists( 'isLicensed' ) AND isLicensed() ) {
-		
+
 			// show the license from wpLicense
 			cc_showLicenseHtml();
-			
+
 		} else {
-			
-			// show copyright 
-			?><p><?php _e( 'Website content', 'commentpress-core' ); ?> &copy; <a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a> <?php echo date('Y'); ?>. <?php _e( 'All rights reserved.', 'commentpress-core' ); ?></p><?php 
-			
+
+			// show copyright
+			?><p><?php _e( 'Website content', 'commentpress-core' ); ?> &copy; <a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a> <?php echo date('Y'); ?>. <?php _e( 'All rights reserved.', 'commentpress-core' ); ?></p><?php
+
 		}
-		
+
 	}
-	
+
 	?>
 
-	
-	
+
+
 </div><!-- /footer_inner -->
 
 </div><!-- /footer -->
