ac800a282728a1a1cc92a1d1b2d70b0dcb047eab
Christian Wach <needle@haystack.co.uk>
2015-06-01 22:35:14 +0100

    clear out globals

--- commentpress-ajax/assets/js/cp-ajax-infinite.dev.js
+++ commentpress-ajax/assets/js/cp-ajax-infinite.dev.js
@@ -13,23 +13,10 @@ This script enables infinite scroll when the CommentPress theme is active.
 
 
 
-// init global vars
-var cpajax_infinite_posts = new Array;
-var cpajax_infinite_comments = new Array;
-var cpajax_comment_form = 0;
-var cpajax_comments_open = 'n';
-
-// page attributes
-var cpajax_post_url = document.location.href;
-var cpajax_post_title = document.title;
-
-/*
-// do we have pushstate?
-if ( window.history ) {
-	console.log( 'ONLOAD' );
-	console.log( window.history );
-}
-*/
+/**
+ * Create CommentPress Infinite sub-namespace
+ */
+CommentPress.infinite = {};
 
 
 
@@ -38,9 +25,192 @@ if ( window.history ) {
 
 
 /**
- * Create CommentPress Infinite sub-namespace
+ * Create settings class
  */
-CommentPress.infinite = {};
+CommentPress.infinite.settings = new function() {
+
+	// store object refs
+	var me = this,
+		$ = jQuery.noConflict();
+
+
+
+	/**
+	 * Initialise CommentPress infinite settings.
+	 *
+	 * This method should only be called once.
+	 *
+	 * @return void
+	 */
+	this.init = function() {
+
+		/*
+		// do we have pushstate?
+		if ( window.history ) {
+			console.log( 'ONLOAD' );
+			console.log( window.history );
+		}
+		*/
+
+		// init post url
+		me.init_post_url();
+
+		// init post title
+		me.init_post_title();
+
+	};
+
+
+
+	/**
+	 * Do setup when jQuery reports that the DOM is ready.
+	 *
+	 * This method should only be called once.
+	 *
+	 * @return void
+	 */
+	this.dom_ready = function() {
+
+	};
+
+
+
+	// init posts array
+	this.posts = new Array;
+
+	/**
+	 * Setter for posts array
+	 */
+	this.set_posts = function( val ) {
+		this.posts = val;
+	};
+
+	/**
+	 * Getter for posts array
+	 */
+	this.get_posts = function() {
+		return this.posts;
+	};
+
+	/**
+	 * Test if post is in posts array
+	 */
+	this.in_posts = function( post_id ) {
+		return $.in_array( post_id, this.posts );
+	};
+
+	/**
+	 * Add to posts array
+	 */
+	this.add_to_posts = function( post_id ) {
+		this.posts.push( post_id );
+	};
+
+
+
+	// init post url
+	this.post_url = '';
+
+	/**
+	 * Init for post url
+	 */
+	this.init_post_url = function() {
+
+		// get initial value
+		this.post_url = document.location.href;
+
+	};
+
+	/**
+	 * Setter for post url
+	 */
+	this.set_post_url = function( val ) {
+		this.post_url = val;
+	};
+
+	/**
+	 * Getter for post url
+	 */
+	this.get_post_url = function() {
+		return this.post_url;
+	};
+
+
+
+	// init post title
+	this.post_title = '';
+
+	/**
+	 * Init for post title
+	 */
+	this.init_post_title = function() {
+
+		// get initial value
+		this.post_title = document.title;
+
+	};
+
+	/**
+	 * Setter for post title
+	 */
+	this.set_post_title = function( val ) {
+		this.post_title = val;
+	};
+
+	/**
+	 * Getter for post title
+	 */
+	this.get_post_title = function() {
+		return this.post_title;
+	};
+
+
+
+	// init post "comments open" setting
+	this.post_comments_open = 'n';
+
+	/**
+	 * Setter for post "comments open" setting
+	 */
+	this.set_post_comments_open = function( val ) {
+		this.post_comments_open = val;
+	};
+
+	/**
+	 * Getter for post "comments open" setting
+	 */
+	this.get_post_comments_open = function() {
+		return this.post_comments_open;
+	};
+
+
+
+	// init comments array
+	this.comments = new Array;
+
+	/**
+	 * Setter for comments array
+	 */
+	this.set_comments = function( val ) {
+		this.comments = val;
+	};
+
+	/**
+	 * Getter for comments array
+	 */
+	this.get_comments = function() {
+		return this.comments;
+	};
+
+	/**
+	 * Add to comments array
+	 */
+	this.add_to_comments = function( post_id, val ) {
+		this.comments[post_id] = val;
+		this.comments.push( post_id );
+	};
+
+} // end CommentPress infinite settings class
 
 
 
@@ -284,8 +454,8 @@ CommentPress.infinite.DOM = new function() {
 		//return;
 
 		// store page data
-		cpajax_post_url = response.url;
-		cpajax_post_title = response.title;
+		CommentPress.infinite.settings.set_post_url( response.url );
+		CommentPress.infinite.settings.set_post_title( response.title );
 
 		/*
 		// $ 1.9 fails to recognise the response as HTML, so
@@ -322,7 +492,7 @@ CommentPress.infinite.DOM = new function() {
 		if ( typeof new_post_prop !== 'undefined' ) {
 
 			// add calling post ID to our array
-			cpajax_infinite_posts.push( post_id );
+			CommentPress.infinite.settings.add_to_posts( post_id );
 
 			// get new post ID
 			new_post_id = new_post_prop.split( '-' )[1];
@@ -333,10 +503,10 @@ CommentPress.infinite.DOM = new function() {
 			//console.log( new_comments_obj );
 
 			// do we have a comment form?
-			if ( cpajax_comment_form !== 0 ) {
+			if ( CommentPress.infinite.commentform.get_comments_form() !== 0 ) {
 
 				// get a copy of the comment form for this post
-				post_comment_form = cpajax_comment_form.clone();
+				post_comment_form = CommentPress.infinite.commentform.get_comments_form().clone();
 
 				// change the form ID so we don't get double submissions
 				$( 'form', post_comment_form ).attr( 'id', 'commentform' );
@@ -345,14 +515,14 @@ CommentPress.infinite.DOM = new function() {
 				$( '#comment_post_ID', post_comment_form ).val( new_post_id );
 
 				// replace stored comments data
-				cpajax_infinite_comments = new_comments_obj.append( post_comment_form );
-				//console.log( 'REPLACE cpajax_infinite_comments:' );
-				//console.log( cpajax_infinite_comments );
+				CommentPress.infinite.settings.set_comments( new_comments_obj.append( post_comment_form ) );
+				//console.log( 'REPLACE CommentPress.infinite.settings.comments:' );
+				//console.log( CommentPress.infinite.settings.get_comments() );
 
 				// set global depending on status
-				cpajax_comments_open = 'n';
+				CommentPress.infinite.settings.set_post_comments_open( 'n' );
 				if ( response.comment_status == 'open' ) {
-					cpajax_comments_open = 'y';
+					CommentPress.infinite.settings.set_post_comments_open( 'y' );
 				}
 
 			}
@@ -503,13 +673,13 @@ CommentPress.infinite.DOM = new function() {
 			item_above.remove();
 
 			// set document title
-			document.title = cpajax_post_title;
+			document.title = CommentPress.infinite.settings.get_post_title();
 
 			// do we have pushstate?
 			if ( window.history ) {
 
 				//console.log( 'PUSHSTATE' );
-				//console.log( 'page_title: ' + cpajax_post_title );
+				//console.log( 'page_title: ' + CommentPress.infinite.settings.get_post_title() );
 
 				// store items as HTML
 				html_to_save = $.trim( context.html() );
@@ -519,13 +689,13 @@ CommentPress.infinite.DOM = new function() {
 
 					{
 						'post_id': post_id,
-						'post_permalink': cpajax_post_url,
-						'page_title': cpajax_post_title,
+						'post_permalink': CommentPress.infinite.settings.get_post_url(),
+						'page_title': CommentPress.infinite.settings.get_post_title(),
 						'html': html_to_save,
 						'comment_status': cp_comments_open
 					},
 					'',
-					cpajax_post_url
+					CommentPress.infinite.settings.get_post_url()
 
 				);
 
@@ -543,12 +713,12 @@ CommentPress.infinite.DOM = new function() {
 
 
 			// get comments
-			comments = cpajax_infinite_comments;
+			comments = CommentPress.infinite.settings.get_comments();
 			//console.log( 'COMMENTS:' );
 			//console.log( comments );
 
 			// do we have a comment form?
-			if ( cpajax_comment_form !== 0 ) {
+			if ( CommentPress.infinite.commentform.get_comments_form() !== 0 ) {
 
 				// disable the comment form
 				addComment.disableForm();
@@ -559,10 +729,10 @@ CommentPress.infinite.DOM = new function() {
 			$( '#comments_sidebar .comments_container' ).replaceWith( comments );
 
 			// set global
-			cp_comments_open = cpajax_comments_open;
+			cp_comments_open = CommentPress.infinite.settings.get_post_comments_open();
 
 			// do we have a comment form?
-			if ( cpajax_comment_form !== 0 ) {
+			if ( CommentPress.infinite.commentform.get_comments_form() !== 0 ) {
 
 				// re-enable the comment form
 				addComment.enableForm();
@@ -641,7 +811,7 @@ CommentPress.infinite.page = new function() {
 	/**
 	 * Init for comment form nonce
 	 */
-	this.init_nonce = function( val ) {
+	this.init_nonce = function() {
 
 		// get initial value from settings object
 		if ( 'undefined' !== typeof CommentpressAjaxInfiniteSettings ) {
@@ -710,7 +880,7 @@ CommentPress.infinite.page = new function() {
 		//console.log( 'WRAPPER GOING DOWN' );
 
 		// kick out if we've already got this one
-		if ( $.in_array( post_id, cpajax_infinite_posts ) ) { return; }
+		if ( CommentPress.infinite.settings.in_posts( post_id ) ) { return; }
 
 		// direction: DOWN
 		//console.log( 'down: ' + post_id );
@@ -892,19 +1062,19 @@ CommentPress.infinite.comments = new function() {
 	 *
 	 * @return void
 	 */
-	thsi.store = function() {
+	this.store = function() {
 
 		// declare vars
 		var post_id;
 
 		// store ID of current post
 		post_id = $( '#wrapper .post' ).prop( 'id' ).split( '-' )[1];
-		//cpajax_infinite_posts.push( post_id );
+		//CommentPress.infinite.settings.add_to_posts( post_id );
 
 		// add new comments data to our array
-		cpajax_infinite_comments[post_id] = $( '#comments_sidebar .comments_container' );
-		//console.log( 'INIT WITH cpajax_infinite_comments:' );
-		//console.log( cpajax_infinite_comments );
+		CommentPress.infinite.settings_add_to_comments( post_id, $( '#comments_sidebar .comments_container' ) );
+		//console.log( 'INIT WITH CommentPress.infinite.settings.comments:' );
+		//console.log( CommentPress.infinite.settings.get_comments() );
 
 	};
 
@@ -957,6 +1127,25 @@ CommentPress.infinite.commentform = new function() {
 
 
 
+	// init comments form
+	this.comments_form = 0;
+
+	/**
+	 * Setter for comments form
+	 */
+	this.set_comments_form = function( val ) {
+		this.comments_form = val;
+	};
+
+	/**
+	 * Getter for comments form
+	 */
+	this.get_comments_form = function() {
+		return this.comments_form;
+	};
+
+
+
 	/**
 	 * Copy and store the comment form
 	 *
@@ -964,21 +1153,27 @@ CommentPress.infinite.commentform = new function() {
 	 */
 	this.store = function() {
 
+		// declare vars
+		var new_form;
+
 		// disable the comment form
 		addComment.disableForm();
 
 		// store a copy of the comment form
-		cpajax_comment_form = $( '#respond_wrapper' ).clone();
+		me.set_comments_form( $( '#respond_wrapper' ).clone() );
+
+		// get new form
+		new_form = me.get_comments_form();
 
 		// change the form ID so we don't get double submissions
-		$( 'form', cpajax_comment_form ).attr( 'id', 'commentform-disabled' );
+		$( 'form', new_form ).attr( 'id', 'commentform-disabled' );
 
 		// remove the extraneous classes
-		if ( $( '#respond_wrapper', cpajax_comment_form ).hasClass( 'cp_force_displayed' ) ) {
-			$( '#respond_wrapper', cpajax_comment_form ).removeClass( 'cp_force_displayed' );
+		if ( $( '#respond_wrapper', new_form ).hasClass( 'cp_force_displayed' ) ) {
+			$( '#respond_wrapper', new_form ).removeClass( 'cp_force_displayed' );
 		}
-		if ( $( '#respond_wrapper', cpajax_comment_form ).hasClass( 'cp_force_closed' ) ) {
-			$( '#respond_wrapper', cpajax_comment_form ).removeClass( 'cp_force_closed' );
+		if ( $( '#respond_wrapper', new_form ).hasClass( 'cp_force_closed' ) ) {
+			$( '#respond_wrapper', new_form ).removeClass( 'cp_force_closed' );
 		}
 
 		// optionally remove from DOM if comments disabled
@@ -1000,6 +1195,7 @@ CommentPress.infinite.commentform = new function() {
 
 
 // do immediate init
+CommentPress.infinite.settings.init();
 CommentPress.infinite.DOM.init();
 CommentPress.infinite.page.init();
 CommentPress.infinite.menu.init();
@@ -1020,6 +1216,7 @@ CommentPress.infinite.comments.init();
 jQuery(document).ready(function($) {
 
 	// trigger DOM ready methods
+	CommentPress.infinite.settings.dom_ready();
 	CommentPress.infinite.DOM.dom_ready();
 	CommentPress.infinite.page.dom_ready();
 	CommentPress.infinite.menu.dom_ready();
