7447ed63d53c3fc66e6a58121cd5208c5c9be989
Christian Wach <needle@haystack.co.uk>
2015-06-24 10:37:25 +0100

    disable blogforum comment sync for CP comments

--- commentpress-multisite/class_commentpress_mu_bp.php
+++ commentpress-multisite/class_commentpress_mu_bp.php
@@ -363,6 +363,29 @@ class CommentpressMultisiteBuddypress {
 
 
 	/**
+	 * Disable comment sync because parent activity items may not be in the same
+	 * group as the comment. Furthermore, CommentPress comments should be read in
+	 * context rather than appearing as if globally attached to the post or page.
+	 *
+	 * @param bool $is_disabled The BP setting that determines blogforum sync
+	 * @return bool $is_disabled The modified value that determines blogforum sync
+	 */
+	public function disable_blogforum_comments( $is_disabled ) {
+
+		// get current blog ID
+		$blog_id = get_current_blog_id();
+
+		// if it's CommentPress-enabled, disable sync
+		if ( $this->db->is_commentpress( $blog_id ) ) return 1;
+
+		// pass through
+		return $is_disabled;
+
+	}
+
+
+
+	/**
 	 * Record the blog activity for the group
 	 * Amended from bp_groupblog_set_group_to_post_activity()
 	 *
@@ -1417,6 +1440,9 @@ class CommentpressMultisiteBuddypress {
 
 		// add pages to the post_types that BP records published activity for
 		//add_filter( 'bp_blogs_record_post_post_types', array( $this, 'record_published_pages' ), 10, 1 );
+
+		// make sure "Allow activity stream commenting on blog and forum posts" is disabled
+		add_action( 'bp_disable_blogforum_comments', array( $this, 'disable_blogforum_comments' ), 20, 1 );
 
 		// override "publicness" of groupblogs
 		add_filter( 'bp_is_blog_public', array( $this, 'is_blog_public' ), 20, 1 );
