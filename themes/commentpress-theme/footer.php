e7f896ef6c100d18fb682fabb7f045af8a7a04b1
Christian Wach <needle@haystack.co.uk>
2018-05-03 10:35:54 +0100

    tidy up footer templates and separate widget containers

--- themes/commentpress-theme/footer.php
+++ themes/commentpress-theme/footer.php
@@ -4,29 +4,17 @@
 
 <div id="footer_inner">
 
-	<?php
-
-	// are we using the page footer widget?
-	if ( ! dynamic_sidebar( 'cp-license-8' ) ) {
-
-		// no - make other provision here
-
-		// compat with wplicense plugin
-		if ( function_exists( 'isLicensed' ) AND isLicensed() ) {
-
-			// show the license from wpLicense
-			cc_showLicenseHtml();
-
-		} else {
-
-			// show copyright
-			?><p><?php _e( 'Website content', 'commentpress-core' ); ?> &copy; <a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a> <?php echo date('Y'); ?>. <?php _e( 'All rights reserved.', 'commentpress-core' ); ?></p><?php
-
-		}
-
-	}
-
-	?>
+	<?php if ( is_active_sidebar( 'cp-license-8' ) ) : ?>
+		<div class="footer_widgets">
+			<?php dynamic_sidebar( 'cp-license-8' ); ?>
+		</div>
+	<?php else : ?>
+		<p><?php echo sprintf(
+			__( 'Website content &copy; %1$s %2$s. All rights reserved.', 'commentpress-core' ),
+			'<a href="' . home_url() . '">' . get_bloginfo( 'name' ) . '</a>',
+			date('Y')
+		); ?></p>
+	<?php endif; ?>
 
 </div><!-- /footer_inner -->
 
