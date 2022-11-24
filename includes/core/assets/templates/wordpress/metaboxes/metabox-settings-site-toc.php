7e20c7c261036b2d6829ec644ffa06da13af34b2
Christian Wach <needle@haystack.co.uk>
2022-11-24 23:52:43 +0000

    Make string properly translatable

--- includes/core/assets/templates/wordpress/metaboxes/metabox-settings-site-toc.php
+++ includes/core/assets/templates/wordpress/metaboxes/metabox-settings-site-toc.php
@@ -9,7 +9,19 @@
 
 ?>
 <!-- includes/core/assets/templates/wordpress/metaboxes/metabox-settings-site-toc.php -->
-<p><?php _e( 'Choose how you want your Table of Contents to appear and function.<br /><strong style="color: red;">NOTE!</strong> When Chapters are Pages, the TOC will always show Sub-Pages, since collapsing the TOC makes no sense in that situation.', 'commentpress-core' ); ?></p>
+<p>
+	<?php esc_html_e( 'Choose how you want your Table of Contents to appear and function.', 'commentpress-core' ); ?><br />
+	<?php
+
+	printf(
+		/* translators: 1: The opening strong tag, 2: The closing strong tag. */
+		__( '%1$sNOTE!%2$s When Chapters are Pages, the TOC will always show Sub-Pages, since collapsing the TOC makes no sense in that situation.', 'commentpress-core' ),
+		'<strong style="color: red;">',
+		'</strong>'
+	);
+
+	?>
+</p>
 
 <table class="form-table">
 
