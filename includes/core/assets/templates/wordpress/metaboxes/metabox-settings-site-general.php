325c50d3d323e66f5b45fd4de3e1dcf35f8fe692
Christian Wach <needle@haystack.co.uk>
2022-11-25 08:51:19 +0000

    Capitalisation

--- includes/core/assets/templates/wordpress/metaboxes/metabox-settings-site-general.php
+++ includes/core/assets/templates/wordpress/metaboxes/metabox-settings-site-general.php
@@ -24,7 +24,7 @@
 
 	<tr>
 		<th scope="row">
-			<label for="cp_reset"><?php esc_html_e( 'Reset options to plugin defaults', 'commentpress-core' ); ?></label>
+			<label for="cp_reset"><?php esc_html_e( 'Reset Settings to defaults', 'commentpress-core' ); ?></label>
 		</th>
 		<td>
 			<input id="cp_reset" name="cp_reset" value="1" type="checkbox" />
